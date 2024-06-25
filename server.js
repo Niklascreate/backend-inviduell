2a3b63e60f06537a2d3976e06cc6018f65dbb032
Adam Rehal <Niklasn89@gmail.com>
2024-06-25 18:19:22 +0200

    fix

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import productsRoutes from './routes/products.js';
 import adminRoute from './routes/admin.js';
-import menuRoute from './routes/updatemenu.js';
 
 const app = express();
 const PORT = 8080;
@@ -10,7 +9,7 @@ app.use(express.json());
 
 app.use('/admin', adminRoute); // Skapa admin via POST /admin/verify
 app.use('/products', productsRoutes); // Hämta produkterna
-app.use('/updatedmenu', menuRoute); // modifiera en produkt
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
